2651de386b61d00aa1c8dab4e6cbfd163bb17e53
Shohin <shohin_6707@mail.ru>
2020-01-21 11:03:41 +0500

    fixes category field

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 const rootEl = document.getElementById('root')
 rootEl.innerHTML = `
 <form data-id="form-sum">
-<input data-id="sum-input">
+<label for="sum-input">Введите сумму: </label>
+<input id="sum-input" data-id="sum-input" type="number">
+<label for="sum-input">Введите сумму: </label>
 <input data-id="category-input">
 <button data-action="add">Добвить</button>
-<div data-id="amount"></div>
+
 </form>
+<div data-id="amount"></div>
 `
 
 const formEl = rootEl.querySelector('[data-id=form-sum]') 
 const inputSumEl = formEl.querySelector('[data-id=sum-input]')
 const inputCategoryEl = formEl.querySelector('[data-id=category-input]')
 const buttonAddEl = formEl.querySelector('[data-action=add]')
-const amountEl = formEl.querySelector('[data-id=amount]')
+const amountEl = rootEl.querySelector('[data-id=amount]')
 
-amountEl.textContent = 'Общяя сумма: '
+amountEl.textContent = 'Общяя сумма: 0'
 let sum = 0;
 buttonAddEl.onclick = (evt) => {
     evt.preventDefault();
     sum+=parseFloat(inputSumEl.value)
     amountEl.textContent = `Общяя сумма: ${sum}`
     inputSumEl.value = ''
+    inputCategoryEl.value = ''
 }
