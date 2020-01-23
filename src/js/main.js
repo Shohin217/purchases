28cfc761fbcdaf5086c92c347cb777176e9f3124
Shohin <shohin_6707@mail.ru>
2020-01-23 15:30:18 +0500

    added buttons up, down and remove

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@ rootEl.innerHTML = `
 <form data-id="form-sum">
 <label for="sum-input">Введите сумму: </label>
 <input id="sum-input" data-id="sum-input" type="number">
-<label for="sum-input">Введите сумму: </label>
-<input data-id="category-input">
+<label for="sum-category">Введите категрию: </label>
+<input id=" sum-category" data-id="category-input">
 <button data-action="add">Добвить</button>
-
 </form>
+<ul data-id="purchases-list"></ul>
 <div data-id="amount"></div>
 `
 
@@ -17,12 +17,56 @@ const inputCategoryEl = formEl.querySelector('[data-id=category-input]')
 const buttonAddEl = formEl.querySelector('[data-action=add]')
 const amountEl = rootEl.querySelector('[data-id=amount]')
 
+const purchasesListEl = rootEl.querySelector('[data-id=purchases-list]')
+
+
+
 amountEl.textContent = 'Общяя сумма: 0'
+
 let sum = 0;
+
 buttonAddEl.onclick = (evt) => {
     evt.preventDefault();
     sum+=parseFloat(inputSumEl.value)
     amountEl.textContent = `Общяя сумма: ${sum}`
+    
+    const category = inputCategoryEl.value
+    const value  = inputSumEl.value
+    const purchasesEl = document.createElement('li');
+    
+    purchasesEl.innerHTML =`
+    Попукпа на сумму ${value}, в категрии ${category} <button data-action="up">UP</button> <button data-action="down">DOWN</button> <button data-action="remove">x</button>
+    `;
+    
+    const purchasesRemove= purchasesEl.querySelector('[data-action=remove]')
+    
+    purchasesRemove.onclick = () =>{
+        purchasesEl.remove('li')
+        sum -= value
+        amountEl.textContent = `Общяя сумма: ${sum}`
+    } 
+
+    purchasesListEl.insertBefore(purchasesEl, purchasesListEl.firstElementChild)
     inputSumEl.value = ''
     inputCategoryEl.value = ''
+    inputSumEl.focus()
+    
+    const buttonUp = document.querySelector('[data-action=up]');
+    const buttonDown = document.querySelector('[data-action=down]');
+    
+    buttonUp.onclick=()=>{
+        if(purchasesEl==purchasesListEl.firstElementChild){
+            purchasesListEl.insertBefore(purchasesEl, null)
+        }else{
+            purchasesListEl.insertBefore(purchasesEl, purchasesEl.previousElementSibling)
+        }
+    }
+
+    buttonDown.onclick=()=>{
+        if(purchasesEl===purchasesListEl.lastElementChild){
+            purchasesListEl.insertBefore(purchasesEl, purchasesListEl.firstElementChild)
+        }else{
+            purchasesListEl.insertBefore(purchasesEl.nextElementSibling, purchasesEl )
+        }
+    }
 }
